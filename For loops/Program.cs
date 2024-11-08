06b68844191d57be5dcec51f27ece23994d1741c
Abdulkadir <qansax30@hotmail.com>
2024-11-08 22:27:41 +0100

    Add project files.

---
+++ For loops/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hi");
+        Console.Write("How many times do you want to eat?:");
+        int loopcounter = Convert.ToInt32(Console.ReadLine());
+
+        if (loopcounter <= 0)
+        {
+            Console.WriteLine("sorry,please enter a value above 0");
+        }
+        else //>= 1
+        {
+            for (int i = 0; i < loopcounter; i++)
+            {
+                Console.WriteLine("Hi");
+            }
+        }
+    }
+}
